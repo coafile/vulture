8621f27b6c9f451999ea248e8c53972c3b52db64
RJ722 <rahul722j@gmail.com>
2017-07-25 11:11:46 +0530

    Add test_release.sh

---
+++ test_release.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+set -euo pipefail
+
+VERSION="$1"
+
+tox
+
+# Check for uncommited changes.
+set +e
+git diff --quiet && git diff --cached --quiet
+retcode=$?
+set -e
+
+if [[ $retcode != 0 ]]; then
+    echo "There are uncommited changes:"
+    git status
+    exit 1
+fi
+
+git pull
+
+# Bump version.
+sed -i -e "s/__version__ = '.*'/__version__ = '$VERSION'/" vulture/core.py
+git commit -am "Update version number to ${VERSION} for release."
+git tag -a "v$VERSION" -m "v$VERSION" HEAD
+
+python setup.py sdist upload -r testpypi
+python setup.py bdist_wheel upload -r testpypi
+
+git push
+git push --tags
